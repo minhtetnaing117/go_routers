6cef660285e254fb9ede0c8487ac746d2ca94b89
minhtetnaing117 <59909903+minhtetnaing117@users.noreply.github.com>
2024-06-30 18:22:10 +0630

    clean code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:go_routers/pages/app_router.dart';
-import 'package:go_routers/pages/details_page.dart';
-import 'package:go_routers/pages/home_page.dart';
-import 'package:go_routers/pages/main_page.dart';
-import 'package:go_routers/pages/second_page.dart';
+
 
 void main() {
   runApp(const MyApp());
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:go_routers/pages/details_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
